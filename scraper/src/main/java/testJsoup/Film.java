a3f8968cfa06e31ab540cb66f3f5d463eb165865
yo <100383242@alumnos.uc3m.es>
2020-12-01 17:17:03 +0100

    read csv, add parse title to date, create film object

---
+++ scraper/src/main/java/testJsoup/Film.java
@@ -0,0 +1,110 @@
+package testJsoup;
+public class Film{
+    //data csv
+    int id;
+    String link;
+    String title;
+    double score;
+    String[]genres;
+    String poster;
+
+    //
+    String[]actors;
+    int year;
+    
+
+    public Film(int id, String link, String title, double score, String[] genres, String poster) {
+		super();
+		this.id = id;
+		this.link = link;
+		this.title = title;
+		this.score = score;
+		this.genres = genres;
+		this.poster = poster;
+	}
+
+
+	public Film(){
+
+    }
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getLink() {
+		return link;
+	}
+
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public double getScore() {
+		return score;
+	}
+
+
+	public void setScore(double score) {
+		this.score = score;
+	}
+
+
+	public String[] getGenres() {
+		return genres;
+	}
+
+
+	public void setGenres(String[] genres) {
+		this.genres = genres;
+	}
+
+
+	public String getPoster() {
+		return poster;
+	}
+
+
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+
+
+	public String[] getActors() {
+		return actors;
+	}
+
+
+	public void setActors(String[] actors) {
+		this.actors = actors;
+	}
+
+
+	public int getYear() {
+		return year;
+	}
+
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+}
--- scraper/src/main/java/testJsoup/Scraper.java
+++ scraper/src/main/java/testJsoup/Scraper.java
@@ -1,32 +1,59 @@
 package testJsoup;
+import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import com.opencsv.CSVReader;
+
 public class Scraper {
+	private static List<Film>films = new ArrayList<Film>();
+	
 	public static final String URL_WIKIE = "https://www.imdb.com/title/tt0111161/";
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		getTitle(URL_WIKIE);
-		//getHRef(URL_WIKIE);
-		//getImgs(URL_WIKIE);
-		//getH4(URL_WIKIE);
-		getSummary(URL_WIKIE);
-		getSummaryItem(URL_WIKIE);
+		readCSV();
+		
+		String url;
+		for(Film film : films) {
+			url =film.getLink();
+			extractDate(film);
+			getSummary(url, film);
+			getSummaryItem(url, film);
+		}
+	}
+
+	private static void readCSV() throws IOException{
+		CSVReader reader = new CSVReader(new FileReader("data.csv"));
+        List<String[]> lines = reader.readAll();
+        
+        
+        for(String[] d : lines) {
+        	int id = Integer.parseInt(d[0]);
+        	String link = d[1];
+        	String title = d[2];
+        	double score = Double.parseDouble(d[3]);
+        	String[]genres = d[4].split("|");
+        	String poster = d[5];
+        	
+        	films.add(new Film(id,link,title,score, genres, poster));
+        }
+        reader.close();
 	}
 	
-	/**
-	 * Este método recupera el título de cualquier página web
-	 * @param url
-	 * @throws IOException
-	 */
-	public static void getTitle(String url) throws IOException{
-		Document doc = Jsoup.connect(url).get();
-		System.out.println(url + ":\t" + doc.title());
-		System.out.println();
+	private static void extractDate(Film film) {
+		String str = film.getTitle();
+		int year = Integer.parseInt(str.substring(str.indexOf("(")+1,str.indexOf(")")));
+		String newTitle = str.substring(0,str.indexOf("(")-1);
+		
+		film.setTitle(newTitle);
+		film.setYear(year);
 	}
 	
 	/**
@@ -34,7 +61,7 @@ public class Scraper {
 	 * @param url
 	 * @throws IOException
 	 */
-	public static void getHRef(String url) throws IOException{
+	public static void getHRef(String url, Film film) throws IOException{
 		Document doc = Jsoup.connect(url).get();
 		System.out.println("a href from:\t" + doc.title());
 		Elements lst = doc.select("a[href]");
@@ -49,7 +76,7 @@ public class Scraper {
 	 * @param url
 	 * @throws IOException
 	 */
-	public static void getImgs(String url) throws IOException{
+	public static void getImgs(String url, Film film) throws IOException{
 		Document doc = Jsoup.connect(url).get();
 		System.out.println("Imgs from:\t" + doc.title());
 		Elements lst = doc.select("img[src~=[\\w\\d\\W]logo[\\w\\d\\W]]");
@@ -64,7 +91,7 @@ public class Scraper {
 	 * @param url
 	 * @throws IOException
 	 */
-	public static void getH4(String url) throws IOException{
+	public static void getH4(String url, Film film) throws IOException{
 		Document doc = Jsoup.connect(url).get();
 		System.out.println("H4 from:\t" + doc.title());
 		//obtenemos todas las etiquetas h4
@@ -80,7 +107,7 @@ public class Scraper {
 	 * @param url
 	 * @throws IOException
 	 */
-	public static void getSummary(String url) throws IOException{
+	public static void getSummary(String url, Film film) throws IOException{
 		Document doc = Jsoup.connect(url).get();
 		System.out.println("Summary from:\t" + doc.title());
 		//obtenemos el div summary_text
@@ -97,7 +124,7 @@ public class Scraper {
 	 * @param url
 	 * @throws IOException
 	 */
-	public static void getSummaryItem(String url) throws IOException{
+	public static void getSummaryItem(String url, Film film) throws IOException{
 		Document doc = Jsoup.connect(url).get();
 		System.out.println("Items from:\t" + doc.title());
 		//obtenemos el div credit_summary_item
