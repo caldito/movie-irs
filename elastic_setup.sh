6b32d4c9d0b8cc3668b82053ed7d764f28ccc460
yo <100383242@alumnos.uc3m.es>
2020-12-16 00:28:30 +0100

    fix aggregattion

--- elastic_setup.sh
+++ elastic_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # create mapping
-curl -X PUT "localhost:9200/films?pretty" -H 'Content-Type: application/json' -d '{"mappings":{"properties":{"id":{"type":"keyword"},"link":{"type":"text"},"title":{"type":"text"},"score":{"type":"double"}, "genres":{"type":"text"},"poster":{"type":"text"},"sinopsis":{"type":"text"},"actors":{"type":"text","fielddata": true},"year":{"type":"integer"}}}}'
+curl -X PUT "localhost:9200/films?pretty" -H 'Content-Type: application/json' -d '{"mappings":{"properties":{"id":{"type":"integer"},"link":{"type":"keyword"},"title":{"type":"keyword"},"score":{"type":"double"}, "genres":{"type":"text"},"poster":{"type":"keyword"},"summary":{"type":"text"},"actors":{"type":"keyword"},"year":{"type":"integer"}}}}'
 
 # index films
 curl -XPOST -H "Content-Type: application/json" "localhost:9200/films/_bulk?pretty" --data-binary "@films.json"
