6350658503aaa486d8c18b2661d3fb18edfdc498
yo <100383242@alumnos.uc3m.es>
2020-12-16 19:09:59 +0100

    update elastic_setup.sh

--- elastic_setup.sh
+++ elastic_setup.sh
@@ -4,4 +4,4 @@
 curl -X PUT "localhost:9200/films?pretty" -H 'Content-Type: application/json' -d '{"mappings":{"properties":{"id":{"type":"integer"},"link":{"type":"keyword"},"title":{"type":"keyword"},"score":{"type":"double"}, "genres":{"type":"text"},"poster":{"type":"keyword"},"summary":{"type":"text"},"actors":{"type":"keyword"},"year":{"type":"integer"}}}}'
 
 # index films
-curl -XPOST -H "Content-Type: application/json" "localhost:9200/films/_bulk?pretty" --data-binary "@films.json"
+curl -XPOST -H "Content-Type: application/json" "localhost:9200/films/_bulk?pretty" --data-binary "@films_scraped.json"
